1493b0544e197a9b12b2f2e0483ff04c60118a0e
Jan Bílek <jan@localazy.com>
2021-07-13 16:19:36 +0200

    Initial Commit

---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by Jan Bilek on 13.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
